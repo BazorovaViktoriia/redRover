77f919cd1c34160108d9d0fc6a8e2a1fa203f9f0
Виктория Базорова <vikitutnet@mail.ru>
2024-09-12 15:00:35 +0300

    Задание 7. Добавлены четыре метода

---
+++ src/SevenLesson.java
@@ -0,0 +1,17 @@
+public class SevenLesson {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int difference(int a, int b) {
+        return a - b;
+    }
+
+    public int multy(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
